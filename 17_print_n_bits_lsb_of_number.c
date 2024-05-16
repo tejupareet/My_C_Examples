bb3957d04e8a9862afb5d84c34287aa5d97c90da
tejupareet <138556343+tejupareet@users.noreply.github.com>
2024-05-16 17:06:09 +0530

    Update 17_print_n_bits_lsb_of_number.c

--- 17_print_n_bits_lsb_of_number.c
+++ 17_print_n_bits_lsb_of_number.c
@@ -1,4 +1,44 @@
-#include<stdio.h>
+/*Name: Teju B Pareet
+Date: 03-07-2022
+Description: WAP to print 'n' bits from LSB of a number
+
+Inputs: -
+
+Sample Execution: 
+Test Case 1:
+Enter the number: 10
+Enter number of bits: 12
+Binary form of 10: 0 0 0 0 0 0 0 0 1 0 1 0 
+
+Test Case 2:
+Enter the number: 15
+Enter number of bits: 4
+Binary form of 15: 1 1 1 1*/
+
+#include <stdio.h>
+void print_bits(int, int);//function dicleration
+int main()
+{
+    int num, n;
+    printf("Enter num, n :\n");
+    scanf("%d%d", &num, &n);
+    if(n<=31)
+    {
+    printf("Binary form of %d:", num);
+    print_bits(num, n);
+    }
+    else
+    printf("n= 0 to 31");
+ }
+void print_bits(int num, int n)//void for non returning out put .
+{
+    for(int i=n-1; i>=0; i--)//loop to get n number of binary digits
+        printf("%d ",1&(num>>i));//shifting right side for masking and &operation 
+
+printf("\n");
+}
+
+/*#include<stdio.h>
 
 int print_n_bit_lsb_of_num(int,int);
 
@@ -22,4 +62,4 @@ int print_n_bit_lsb_of_num(int num, int n)
 	{
 		printf("%d ",res=(1 & (num>>i)));
 	}
-}
+}*/
