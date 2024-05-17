6a44d9c908f8299257d4b9fa3744a1a6c3d9b87c
tejupareet <138556343+tejupareet@users.noreply.github.com>
2024-05-17 15:54:48 +0530

    Add files via upload

--- 25_second_large_in_array.c
+++ 25_second_large_in_array.c
@@ -1,4 +1,82 @@
+/*Name: Teju B Pareet
+
+Description: WAP to find 2nd largest element in an array
+
+Inputs: -
+Sample Execution:
+Test Case 1:
+Enter the size of the Array : 5
+Enter the elements into the array: 5 1 4 2 8
+Second largest element of the array is 5
+Test Case 2:
+Enter the size of the Array : 4
+Enter the elements into the array: 66 22 11 3
+Second largest element of the array is 22*/
+
 #include<stdio.h>
+
+int sec_largest(int [], int);
+
+int main()
+{
+    int n, ret;
+    
+    //Read size from the user
+    printf("Enter the size of the array :");
+    scanf("%d", &n);
+    
+    int arr[n];
+    
+    //Read elements into the array
+    printf("enter the array elements\n");
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+    //funtion call
+    ret = sec_largest(arr, n);
+    
+    printf("Second largest element of the array is %d\n", ret);
+}
+int sec_largest(int arr[],int n)//function definition
+{
+    int s_large,f_large;//local variables
+    if(arr[0] == arr[1] )
+    {
+        s_large=0;
+        f_large=arr[1];
+    }
+    else if(arr[0] > arr[1])//condtion to check which is greater and storing it to the 1st large & 2nd large number for first 2 index
+    {
+         f_large = arr[0] ;
+         s_large = arr[1];
+    }
+    else
+    {
+        f_large = arr[1];
+        s_large = arr[0];
+    }
+    for( int i=2; i <= n-1; i++ )//loop to check remaing index values
+    {
+        if( arr[i] > f_large )
+        {
+            s_large = f_large;
+            f_large = arr[i];
+        }
+        else if( arr[i] > s_large )
+        {
+            s_large = arr[i];
+        }
+    }
+        return s_large;//returning second largest to called function
+}
+
+
+
+
+
+
+/*#include<stdio.h>
 
 int sec_large(int arr[],int n );
 
@@ -43,4 +121,4 @@ int sec_large(int arr[], int n)
 		}
 	}
 	return arr[1];
-}
+}*/
